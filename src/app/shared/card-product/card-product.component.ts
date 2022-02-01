b0dc83c6de21ccf344edc5370b442b068eb5d529
Luis Rivero <luis.arh.1109@gmail.com>
2022-01-31 21:54:16 -0400

    feat: agregadas funciones en el html

--- src/app/shared/card-product/card-product.component.ts
+++ src/app/shared/card-product/card-product.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Router } from '@angular/router';
 import { faHeart } from '@fortawesome/free-regular-svg-icons';
 import { faCartPlus, faHeart as heartSolid } from '@fortawesome/free-solid-svg-icons';
 
@@ -19,21 +20,32 @@ export class CardProductComponent implements OnInit {
   }
   product: any;
 
-  constructor() { }
+  constructor(
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
   }
 
   favoriteAdd() {
+    console.log('fav add')
     this.favAction.emit(true);
   }
 
   favoriteDel() {
+    console.log('fav del')
     this.favAction.emit(false);
   }
 
   addCartFn() {
-    this.addCart.emit(this.product.id);
+    console.log('add cart')
+    // this.addCart.emit(this.product.id);
+    this.addCart.emit(1);
+  }
+
+  goToDetails() {
+    // this.router.navigate(['product', this.product.id]);
+    this.router.navigate(['product', 1]);
   }
 
 }
